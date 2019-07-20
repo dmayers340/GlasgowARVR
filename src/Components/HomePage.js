2db06df621866dc5506a5c8c936da1df61ebb6fa
Deb <dmayers340@gmail.com>
2019-07-20 17:29:05 +0100

    Added a new logo

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { TwitterTimelineEmbed } from 'react-twitter-embed';
 import { NavLink  } from 'react-router-dom';
 
+const backgroundImg = 'img/NewLogo.png';
+
 export const HomePage = () => {
     return (
-        <div className = 'homepage'>
+        <div className = 'homepage' style ={{backgroundImage:"url("+backgroundImg+")", backgroundRepeat  : 'no-repeat',
+        backgroundPosition: 'center'}}>
             <h1>Glasgow ARVR</h1>
             <NavLink  to={'/events'} className="nav-link">Events</NavLink >
             <TwitterTimelineEmbed
+                className="twitter"
                 sourceType="profile"
                 screenName="ARVRGlasgow"
                 options={{height: 600}}
