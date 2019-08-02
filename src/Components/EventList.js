e6f856a0c6d508fe7488df1e747ea8e725aed0e9
Deb <dmayers340@gmail.com>
2019-08-02 19:10:58 +0100

    Added August and September events, as well as phoots to the front page

--- src/Components/EventList.js
+++ src/Components/EventList.js
@@ -1,25 +1,46 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
-import { events } from '../config/eventlist.json';
+import { events, pastevents } from '../config/eventlist.json';
 
 export const EventList = () => {
     return (
-        <div className = "card-row">
-            { events.map(event => {
-                return(
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Body>
-                        <Card.Img variant="top" src={event.imageUrl} />
-                        <Card.Title>{event.eventName}</Card.Title>
-                            <Card.Text>{event.description}</Card.Text>
-                            <Card.Text>{event.date}</Card.Text>
-                            <Card.Text>{event.time}</Card.Text>
-                            <Card.Text>{event.location}</Card.Text>
-                            { event.url ? <a href = {event.url}>Sign up on Eventbrite!</a> : ""}
-                        </Card.Body>
-                    </Card>
-                )
-            })}
-        </div>
+        <>
+            <h2>Upcoming Events</h2>
+                <div className = "card-row">
+                    { events.map(event => {
+                        return(
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Body>
+                                <Card.Img variant="top" src={event.imageUrl} />
+                                <Card.Title>{event.eventName}</Card.Title>
+                                    <Card.Text>{event.description}</Card.Text>
+                                    <Card.Text>{event.date}</Card.Text>
+                                    <Card.Text>{event.time}</Card.Text>
+                                    <Card.Text>{event.location}</Card.Text>
+                                    { event.url ? <a href = {event.url}>Sign up on Eventbrite!</a> : ""}
+                                </Card.Body>
+                            </Card>
+                        )
+                    })}
+                </div>
+            <h2> Past Events </h2>
+            <div className = "card-row">
+                { pastevents.map(event => {
+                    return(
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Body>
+                            <Card.Img variant="top" src={event.imageUrl} />
+                            <Card.Title>{event.eventName}</Card.Title>
+                                <Card.Text>{event.description}</Card.Text>
+                                <Card.Text>{event.date}</Card.Text>
+                                <Card.Text>{event.time}</Card.Text>
+                                <Card.Text>{event.location}</Card.Text>
+                                { event.url ? <a href = {event.url}>Sign up on Eventbrite!</a> : ""}
+                            </Card.Body>
+                        </Card>
+                    )
+                })}
+            </div>
+        </>
     )
 }
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import { TwitterTimelineEmbed } from 'react-twitter-embed';
 import { NavLink  } from 'react-router-dom';
-
-const backgroundImg = 'img/NewLogo.png';
+import { Col, Row } from 'react-bootstrap';
 
 export const HomePage = () => {
     return (
-        <div className = 'homepage' style ={{backgroundImage:"url("+backgroundImg+")", backgroundRepeat  : 'no-repeat',
-        backgroundPosition: 'center'}}>
+        <>
             <h1>Glasgow ARVR</h1>
-            <NavLink  to={'/events'} className="nav-link">Events</NavLink >
-            <TwitterTimelineEmbed
-                className="twitter"
-                sourceType="profile"
-                screenName="ARVRGlasgow"
-                options={{height: 600}}
-            />
-        </div>
+            <h3> <NavLink  to={'/events'} className="nav-link">Events</NavLink ></h3>
+            <Row>
+                <Col>
+                    <TwitterTimelineEmbed
+                        className="twitter"
+                        sourceType="profile"
+                        screenName="ARVRGlasgow"
+                        options={{height: 600}}
+                    />
+                </Col>
+                <Col sm><img className = 'image-row' alt="Glasgow ARVR Logo" src={process.env.PUBLIC_URL + '/img/glasgowarvr.png'} /></Col>
+            </Row>     
+            <Row>
+                <Col sm><img className = 'image-row' alt="Glasgow ARVR New Logo" src={process.env.PUBLIC_URL + '/img/NewLogo.png'} /></Col>
+                <Col sm><img className = 'image-row' alt="Two people with VR headsets on at the Glasgow ARVR July Meetup Collider " src={process.env.PUBLIC_URL + '/img/ARVRGlasgow_Collider.jpg'} /></Col>
+            </Row>                                     
+        </>
     )
 }
