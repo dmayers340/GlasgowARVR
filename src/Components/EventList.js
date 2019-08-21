ac68248168327ad94f57deccb9c56d7b3fa88ddb
Deb <dmayers340@gmail.com>
2019-08-21 21:33:59 +0100

    Added some styling to event list to be readable on mobile, and added redirects file

--- src/Components/EventList.js
+++ src/Components/EventList.js
@@ -1,50 +1,56 @@
 import React from 'react';
-import { Card, ListGroupItem, ListGroup } from 'react-bootstrap';
+import { Card, Col, ListGroupItem, ListGroup, Row } from 'react-bootstrap';
 import { events, pastevents } from '../config/eventlist.json';
 
 export const EventList = () => {
     return (
         <div className = 'page-container'>
             <h2>Upcoming Events</h2>
-                <div className = "card-row">
+                <Row>
                     { events.map(event => {
-                        return(
-                            <Card style={{ width: '18rem' }}>
-                                <Card.Body>
-                                <Card.Img variant="top" src={event.imageUrl} />
-                                <Card.Title>{event.eventName}</Card.Title>
-                                <ListGroup className="list-group-flush">
-                                    <ListGroupItem>{event.description}</ListGroupItem>
-                                    <ListGroupItem>{event.date}</ListGroupItem>
-                                    <ListGroupItem>{event.time}</ListGroupItem>
-                                    <ListGroupItem>{event.location}</ListGroupItem>
-                                    { event.url ? <a href = {event.url}>Sign up on Eventbrite!</a> : ""}
-                                </ListGroup>
-                                </Card.Body>
-                            </Card>
+                        return (
+                            <Col>
+                                <Card style={{ width: '20rem' }}>
+                                    <Card.Body>
+                                        <Card.Img variant="top" src={event.imageUrl} />
+                                        <Card.Title>{event.eventName}</Card.Title>
+                                        <Card.Text>
+                                            <ListGroup className="list-group-flush">
+                                                <ListGroupItem>{event.description}</ListGroupItem>
+                                                <ListGroupItem>{event.date}</ListGroupItem>
+                                                <ListGroupItem>{event.time}</ListGroupItem>
+                                                <ListGroupItem>{event.location}</ListGroupItem>
+                                                { event.url ? <a href = {event.url}>Sign up on Eventbrite!</a> : ""}
+                                            </ListGroup>
+                                        </Card.Text> 
+                                    </Card.Body>
+                                </Card>
+                            </Col> 
                         )
                     })}
-                </div>
+                </Row>
             <h2> Past Events </h2>
-            <div className = "card-row">
+            <Row>
                 { pastevents.map(event => {
                     return(
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Body>
-                                <Card.Img variant="top" src={event.imageUrl} />
-                                <Card.Title>{event.eventName}</Card.Title>
-                                <ListGroup className="list-group-flush">
-                                    <ListGroupItem>{event.description}</ListGroupItem>
-                                    <ListGroupItem>{event.date}</ListGroupItem>
-                                    <ListGroupItem>{event.time}</ListGroupItem>
-                                    <ListGroupItem>{event.location}</ListGroupItem>
-                                    { event.url ? <a href = {event.url}>Sign up on Eventbrite!</a> : ""}
-                                </ListGroup>
-                            </Card.Body>
-                        </Card>
+                        <Col>
+                            <Card style={{ width: '20rem' }}>
+                                <Card.Body>
+                                    <Card.Img variant="top" src={event.imageUrl} />
+                                    <Card.Title>{event.eventName}</Card.Title>
+                                    <ListGroup className="list-group-flush">
+                                        <ListGroupItem>{event.description}</ListGroupItem>
+                                        <ListGroupItem>{event.date}</ListGroupItem>
+                                        <ListGroupItem>{event.time}</ListGroupItem>
+                                        <ListGroupItem>{event.location}</ListGroupItem>
+                                        { event.url ? <a href = {event.url}>Sign up on Eventbrite!</a> : ""}
+                                    </ListGroup>
+                                </Card.Body>
+                            </Card>
+                        </Col>
                     )
                 })}
-            </div>
+                </Row>
         </div>
     )
 }
