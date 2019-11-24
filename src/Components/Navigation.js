90b64a2578bebda32242bb2866b5fa62d8af07af
Deb <dmayers340@gmail.com>
2019-11-24 11:51:19 +0000

    Changed to make navbar collapse

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import React, { useState } from 'react';
 import { Switch, Route, NavLink  } from 'react-router-dom';
 import { HomePage } from './HomePage';
 import { EventList } from './EventList';
@@ -9,29 +8,43 @@ import { CodeOfConduct } from './CodeOfConduct';
 import DecAR from './DecAR';
 
 export const Navigation = () => {
+    
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleNavbar = () => {
+        setIsOpen(!isOpen);
+    }
+
+    const collapseClass = isOpen ? 'collapse navbar-collapse' : 'collapse navbar-collapse show';
+    const toggleClass = isOpen ? 'navbar-toggler navbar-toggler-right collapsed' : 'navbar-toggler navbar-toggler-right';
+ 
     return (
         <>
-        <Navbar collapseOnSelect  bg="light" expand="lg">
-                <Navbar.Brand href="/"> 
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class = "navbar-brand" href="/"> 
                     <img
                         src="/img/GlasgowARVRImage.png"
                         height="30"
                         className="d-inline-block align-top"
                         alt="Glasgow ARVR Logo"
                     />
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <NavLink  to={'/'} className="nav-link"> Home </NavLink >
-                        <NavLink  to={'/events'} className="nav-link">Events</NavLink >
-                        <NavLink  to={'/codeofconduct'} className="nav-link">Code of Conduct</NavLink>
-                        <NavLink  to={'/contact'} className="nav-link">Contact</NavLink>
-                        <NavLink  to={'/ar'} className="nav-link">AR</NavLink>
+                </a>
+                
+                <button onClick={toggleNavbar} className={`${toggleClass}`} type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon" />
+                </button>               
+                
+                <div className={`${collapseClass}`} id="navbarResponsive">
+                    <ul class="navbar-nav mr-auto">
+                        <NavLink onClick={toggleNavbar} to={'/'} className="nav-link"> Home </NavLink >
+                        <NavLink onClick={toggleNavbar} to={'/events'} className="nav-link">Events</NavLink >
+                        <NavLink onClick={toggleNavbar} to={'/codeofconduct'} className="nav-link">Code of Conduct</NavLink>
+                        <NavLink onClick={toggleNavbar} to={'/contact'} className="nav-link">Contact</NavLink>
+                        <NavLink onClick={toggleNavbar}  to={'/ar'} className="nav-link">AR</NavLink>
                         <a className ='right-side' href ="https://twitter.com/ARVRGlasgow"><FontAwesomeIcon pull = "right" size="lg" icon={['fab', 'twitter']} /></a>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>        
+                    </ul>
+                </div>
+            </nav>        
 
             <Switch>
                 <Route exact path='/' component={HomePage} />
