c8a1997e96af9fadd14084c4bfa91bd0f7fa8848
Deb <dmayers340@gmail.com>
2019-11-24 20:21:39 +0000

    Added Christmas Tree

--- src/Components/DecemberAR.js
+++ src/Components/DecemberAR.js
@@ -13,6 +13,7 @@ const DecemberAR = () => {
         <a-scene embedded arjs="sourceType: webcam;">
             <a-obj-model src="models/cone_adam_marc_williams.obj" mtl="models/materials.mtl" position="0 1.35 0" scale="3 3 3"></a-obj-model>
             <a-obj-model src="models/High-end_headset_01.obj" mtl="models/High-end_headset_01.mtl" position="0 0.5 0" scale="0.1 0.1 0.1"></a-obj-model>
+            <a-obj-model src="models/ChristmasTree.obj" mtl="models/ChristmasTree.mtl" position="1 0 1"></a-obj-model>
             <a-marker-camera preset="hiro"></a-marker-camera>
         </a-scene>
     )
